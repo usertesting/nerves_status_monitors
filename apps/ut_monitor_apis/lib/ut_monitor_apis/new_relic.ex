99d59b37f2ac60ae8465e69f8308af7642418947
Andrew Selder <aselder@usertesting.com>
2016-11-01 10:43:05 -0700

    Handle errors while retrieving apdex data.

--- apps/ut_monitor_apis/lib/ut_monitor_apis/new_relic.ex
+++ apps/ut_monitor_apis/lib/ut_monitor_apis/new_relic.ex
@@ -6,17 +6,18 @@ defmodule UtMonitorApis.NewRelic do
   @new_relic_app_id "348677"
   
   plug Tesla.Middleware.BaseUrl, "https://api.newrelic.com/v2"
-  # Move API Key to config variable
   plug Tesla.Middleware.Headers, %{"X-Api-Key" => Application.get_env(:ut_monitor_apis, :new_relic).api_key}
   plug Tesla.Middleware.JSON
   
-  def get_apdex_values do
-    get_apdex_data.body
-    |> Map.get("metric_data")
-    |> Map.get("metrics")
-    |> Enum.find(fn(x) -> x["name"] == "Apdex" end)
-    |> Map.get("timeslices")
-    |> Enum.map(fn(x) -> calculate_apdex(x["values"]) end)
+  def get_apdex_values do   
+    case get_apdex_data do
+      %Tesla.Env{body: body, status: 200} ->
+        {:ok, parse_apdex_values(body)}
+      %Tesla.Env{body: body} ->
+        {:error, body}
+      _ ->
+        {:error, "Unknown Error"}
+    end
   end
   
   defp get_apdex_data(num_slices \\ 72) do
@@ -29,6 +30,15 @@ defmodule UtMonitorApis.NewRelic do
     ])
   end
 
+  defp parse_apdex_values(body) do
+    body
+    |> Map.get("metric_data")
+    |> Map.get("metrics")
+    |> Enum.find(fn(x) -> x["name"] == "Apdex" end)
+    |> Map.get("timeslices")
+    |> Enum.map(fn(x) -> calculate_apdex(x["values"]) end)
+  end
+  
   defp calculate_apdex(%{"s" => satisfied, "t" => tolerating, "f" => failing}) do
     (satisfied + tolerating /2)/(satisfied + tolerating + failing)
   end
